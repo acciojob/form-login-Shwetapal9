d8b6ced60350e7efda9dbec47511b4db4aba198e
Shweta pal <51901918+Shwetapal9@users.noreply.github.com>
2023-05-23 17:49:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
-
+// let formid= document.getElementById("form1");
+	let fname = document.getElementsByTagName("input")[0];
+	let lname = document.getElementsByTagName("input")[1];
+	// console.log(fname.value, lname.value); 
+	alert(`${fname.value} ${lname.value}`);
+	
 }
